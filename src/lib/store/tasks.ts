2a5a8a5db303764d4fbf7c858d0f46d43809e348
Hadi-Bakhshi <hadibakhshi277@gmail.com>
2024-07-18 22:20:05 +0330

    feat: add task store
    add a new store to save tasks

---
+++ src/lib/store/tasks.ts
@@ -0,0 +1,50 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { v4 as uuidv4 } from 'uuid';
+import { Category, CategoryState } from './categories';
+import toast from 'react-hot-toast';
+
+export type Task = {
+  id: string;
+  category: Category;
+  date: Date;
+  description: string;
+  from: string;
+  to: string;
+  title: string;
+};
+
+export type TaskState = {
+  tasks: Task[];
+};
+
+export type TaskAction = {
+  add: (data: Omit<Task, 'id'>) => void;
+  stop: (id: string, to: string) => void;
+  delete: (id: string) => void;
+};
+
+export const useTaskStore = create<TaskState & TaskAction>()(
+  persist(
+    (set, get) => ({
+      tasks: [],
+      add: (data) => {
+        const clonedState = structuredClone(get().tasks);
+        clonedState.push({
+          id: uuidv4(),
+          ...data,
+        });
+        set(() => ({ tasks: clonedState }));
+      },
+      delete: (id) => set((state) => ({ tasks: state.tasks.filter((item) => item.id !== id) })),
+      stop: (id, to) => {
+        const clonedState = structuredClone(get().tasks);
+        const foundTask = clonedState.find((task) => task.id === id);
+        if (!foundTask) return toast.error("Oh sorry, couldn't find task with this information");
+        foundTask.to = to;
+        set(() => ({ tasks: clonedState }));
+      },
+    }),
+    { name: 'task-store' }
+  )
+);
